b8d9e797bb5beacab08a9e246552f0c160b1c441
Joseph Rose <joeyrose1999@gmail.com>
2020-01-04 16:28:04 -0800

    Added headers to api call

--- app.py
+++ app.py
@@ -22,7 +22,15 @@ def data():
       partOfSeason = request.form['partOfSeason']
       shotClock = request.form['shotClock']
       location = request.form['location']
-
+      headers = {
+      'Host': 'stats.nba.com',
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0',
+      'Accept': 'application/json, text/plain, */*',
+      'Accept-Language': 'en-US,en;q=0.5',
+      'Referer': 'https://stats.nba.com/',
+      'Accept-Encoding': 'gzip, deflate, br',
+      'Connection': 'keep-alive',
+      }
       arr = []
       indexArray = [2, 3, 4, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 19, 20, 21, 22, 24, 25, 26, 27]
       for player in playersArray:
@@ -30,7 +38,7 @@ def data():
 
          ID = player[0].get('id')
 
-         stats = playerdashboardbyclutch.PlayerDashboardByClutch(per_mode_detailed = 'PerGame', player_id = ID, season = season, season_type_playoffs = partOfSeason, shot_clock_range_nullable = shotClock, location_nullable = location)
+         stats = playerdashboardbyclutch.PlayerDashboardByClutch(per_mode_detailed = 'PerGame', player_id = ID, season = season, season_type_playoffs = partOfSeason, shot_clock_range_nullable = shotClock, location_nullable = location, headers = headers)
          if gameScenario == "last10_sec3_point_player_dashboard":
             jsonString = stats.last10_sec3_point_player_dashboard.get_json()
          if gameScenario == "last30_sec3_point_player_dashboard":
