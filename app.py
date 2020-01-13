498c0af246eab74481d843743ec95d16257ac57b
Joseph Rose <joeyrose1999@gmail.com>
2020-01-13 14:29:14 -0800

    Big Update

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from operator import itemgetter
 import os 
 app = Flask(__name__)
 
+application = app
+
 @app.route('/')
 def index():
    return render_template('index.html')
@@ -23,15 +25,6 @@ def data():
       partOfSeason = request.form['partOfSeason']
       shotClock = request.form['shotClock']
       location = request.form['location']
-      headers = {
-      'Host': 'stats.nba.com',
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',
-      'Accept': 'application/json, text/plain, */*',
-      'Accept-Language': 'en-US,en;q=0.5',
-      'Referer': 'https://github.com/',
-      'Accept-Encoding': 'gzip, deflate, br',
-      'Connection': 'keep-alive',
-      }
       arr = []
       indexArray = [2, 3, 4, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 19, 20, 21, 22, 24, 25, 26, 27]
       for player in playersArray:
@@ -39,7 +32,7 @@ def data():
 
          ID = player[0].get('id')
 
-         stats = playerdashboardbyclutch.PlayerDashboardByClutch(per_mode_detailed = 'PerGame', player_id = ID, season = season, season_type_playoffs = partOfSeason, shot_clock_range_nullable = shotClock, location_nullable = location, headers = headers)
+         stats = playerdashboardbyclutch.PlayerDashboardByClutch(per_mode_detailed = 'PerGame', player_id = ID, season = season, season_type_playoffs = partOfSeason, shot_clock_range_nullable = shotClock, location_nullable = location, timeout = 500)
          if gameScenario == "last10_sec3_point_player_dashboard":
             jsonString = stats.last10_sec3_point_player_dashboard.get_json()
          if gameScenario == "last30_sec3_point_player_dashboard":
@@ -62,5 +55,4 @@ def data():
       realJSON["headers"] = shortLabelsArray
       return jsonify(realJSON)
 if __name__ == '__main__':
-   # Bind to PORT if defined, otherwise default to 5000.
     app.run(debug = True)
